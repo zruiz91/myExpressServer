c40276a23e4139cf9119107452cda829c5f6ae4d
Zach Ruiz <powersbooth10@gmail.com>
2023-04-18 16:58:07 -0500

    added a response to the server code.

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', function(req, res) {
+    console.log(req);
+    res.send("<h1>hello</h1>");
+})
+
+app.listen(3000, function () {
+    console.log('listening on port 3000');
+});
