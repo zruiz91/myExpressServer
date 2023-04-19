8468201c3b91d3a27fe85b6e71ba955d01c2e8cf
Zach Ruiz <powersbooth10@gmail.com>
2023-04-19 10:32:09 -0500

    added some routes to the the server to practice express with node.

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function(req, res) {
     res.send("<h1>hello</h1>");
 })
 
+app.get("/contact", function(req, res) {
+    res.send("<h1>Contact me at: Your Mom's House</h1>");
+})
+
+app.get("/about", function(req, res) {
+    res.send("<h1>Hello, my name is Zach and I'm a full stack software engineer!</h1>");
+})
+
+app.get("/hobbies", function(req, res) {
+    res.send("<h1>Hobbies include: baseball, beer, beach stuff</h1>")
+})
+
 app.listen(3000, function () {
     console.log('listening on port 3000');
 });
